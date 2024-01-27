8739c69a40d6a6cbe86b905d68260b820bb51955
blurk <nsinh6745@gmail.com>
2024-01-27 09:13:24 +0700

    feat(keymap): open explore

--- init.lua
+++ init.lua
@@ -303,6 +303,7 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<C-n>', vim.cmd.nohlsearch, { desc = 'No search highlight' })
+vim.keymap.set('n', '<leader>ex', vim.cmd.Ex, {desc = 'Open netrw'})
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
